d900d101e525e7321ecde1591fdde45c38c07dee
Cold Cat <yuchengzhixia@gmail.com>
2017-03-09 02:47:16 +0800

    Introduction to iOS 10, Xcode 8 and Swift 3

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 夏语诚 on 2017/3/9.
+//  Copyright © 2017年 Banana Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userIsInTheMiddleOfTyping = false
+ 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+
+    @IBOutlet weak var display: UILabel!
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+}
+
